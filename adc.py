a6414f9141547d6d0fe313a7379fc379e21d8aba
Hiran H E Jayaweera <hiran@flash.uoc>
2017-04-13 22:06:14 +0530

    Made Visualisation class independant of functionality of adc

--- adc.py
+++ adc.py
@@ -19,39 +19,48 @@ class Adc(object):
         return adc_val
 
 class Visualize_adc(object):
-    adc=Adc()   
-    def __init__(self,adc=Adc()):
-        self.adc=adc
-    def plot(self,ang,dig,adc=adc):
-        t=range(0,len(ang))
+    def __init__(self):
+        pass
+
+    def plot(self,signals):
+        t_analog = signals[0]
+        analog_signal = signals[1]
+        t_digital = signals[2]
+        digital_signal = signals[3]
+        adc = signals[4]
         fig, ax1 = plt.subplots()
         ax1.set_ylabel('Voltage',color='b')
         ax1.tick_params('y',colors='b')
-        ax1.plot(t,ang,'-b',zorder=10)
+        ax1.plot(t_analog,analog_signal,'-b',zorder=10)
         ax1.set_xlabel('Time')
         ax1.set_ylim([adc.lower_ref_voltage,adc.upper_ref_voltage])
         ax2 = ax1.twinx()
         ax2.set_ylim([0,2**adc.no_of_bits-1])
 
-        ax2.step(t[1::10],dig[1::10],'r',zorder=10)
+        ax2.step(t_digital,digital_signal,'r',zorder=10)
         ax2.set_ylabel('ADC value',color='r')
         ax2.tick_params('y',colors='r')
         ax2.grid(True, zorder=5)
         plt.title(adc.get_config())
         plt.show()
+ 
         
 class System(object):
     """This class create signals and do the AD conversion using ADC class
     """
-    analog_signal=0;
-    digital_signal=0;
     analog_sampling_rate=500
     adc=Adc()
     def __init__(self,analog_sampling_rate=analog_sampling_rate,adc=adc):
         self.analog_sampling_rate=analog_sampling_rate
         self.adc=adc
-    def get_signal():
-        return np.arange(0,5,.1)
+    def get_signal(self,analog_signal):
+        t_analog=np.linspace(0, 1, self.analog_sampling_rate)
+        under_sampling_step =int(np.ceil(self.analog_sampling_rate/self.adc.sampling_rate))
+        
+        digital_signal=self.adc.get_adc_val(analog_signal[1::under_sampling_step])
+        t_digital=t_analog[1::under_sampling_step]
+        
+        return [t_analog, analog_signal,t_digital,digital_signal,self.adc]
 
 a=Adc(4)
 s=System(500,a)
@@ -59,6 +68,5 @@ s=System(500,a)
 x = np.linspace(0, 1, s.analog_sampling_rate)
 x = np.abs(np.sin(4*np.pi*x)*np.exp(-5*x))*8
 y = a.get_adc_val(x);
-
-v=Visualize_adc(a);
-v.plot(x,y,a)    
+v=Visualize_adc();
+v.plot(s.get_signal(x))    
